8ad545132840ecae95d694c8d27cfd9b9bf1d925
tranvinh146 <vinh.tt@geekup.vn>
2022-10-01 07:12:14 +0700

    add config goerli

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@typechain/hardhat";
 import "@nomiclabs/hardhat-waffle";
-import "@nomiclabs/hardhat-ethers";
+// import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-etherscan";
 import "dotenv/config";
 import "hardhat-deploy";
